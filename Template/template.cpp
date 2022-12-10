d4a6131261af12cb26fedd3059731a83a98b56ee
hvgupta <72889146+hvgupta@users.noreply.github.com>
2022-12-10 14:25:05 +0800

    Create template.cpp

---
+++ Template/template.cpp
@@ -0,0 +1,12 @@
+#include "../file_reader.h"
+using namespace std;
+
+int main(){
+    vector<string> file = get_lines("input.txt");
+    vector<string>::const_iterator data = file.begin();
+
+    while(data != file.end()){
+        
+        data++;
+    }
+}
