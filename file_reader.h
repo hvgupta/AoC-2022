033fe7edae254fecd6265fa7a1760e1301f121ed
hvgupta <72889146+hvgupta@users.noreply.github.com>
2022-12-16 18:28:38 +0800

    Update file_reader.h

--- file_reader.h
+++ file_reader.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <sstream>
+
 
 std::vector<std::string> get_lines(std::string file){
     std::ifstream fin(file);
@@ -14,6 +16,24 @@ std::vector<std::string> get_lines(std::string file){
     }
 
     return file_content;
+}
+
+template <typename T>
+T int_return(std::string line){
+    int split_pos;
+    std::stringstream num_convert;
+    num_convert << line;
+    std::string temp;
+    int num;
+    T array;
+    while (!num_convert.eof()){
+        num_convert >> temp;
+        if (std::stringstream(temp) >> num){
+            array.push_back(num);
+        }
+        temp = "";
+    }
+    return array;
 }
 
 #endif
